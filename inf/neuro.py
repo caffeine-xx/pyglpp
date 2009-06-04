b941fcbce057d6b008a977af668afd3d1e9e59da
Daniel Cook <dc1108@doc.ic.ac.uk>
2009-06-04 19:45:23 +0100

    bizarre shit is NOT working at the moment.

--- inf/neuro.py
+++ inf/neuro.py
@@ -3,9 +3,20 @@ import scipy.optimize as opt
 import random as rd
 import math
 from memoize import *
+from numpy.random import poisson
 
 def cos_stimulus(time,period):
   return np.asarray(map(lambda t: math.cos(t*math.pi*2/period)**2,range(0,time)))
+
+def poiss_train(theta,stim,delta,time,size):
+  spikes = np.zeros(time)
+  data = data_slicer(size,stim,spikes)
+  inten = lambda t: logI(t, theta, data)
+  for t in range(size+1,time):
+    lam = inten(t-1)
+    spikes[t] = (poisson(delta*lam,1)>0)
+  return spikes
+    
 
 def spike_train(stim):
   return np.asarray(map(lambda t: rd.random() < t,stim))
@@ -15,7 +26,7 @@ def spike_times(train):
 
 def cos_filter(size):
   base = map(lambda i: i * math.pi / (2*size), range(0,size))
-  return map(lambda i: math.cos(i), base)
+  return np.array(map(lambda i: math.cos(i), base))
 
 def rand_filter(size):
   return [rd.random() for i in range(size)]
@@ -24,14 +35,14 @@ def rand_filter(size):
 def data_slicer(size, stim, spikes):
   start = lambda t: max(t-size,0)
   sliced =lambda t: np.concatenate(([1], stim[start(t):t], spikes[start(t):t])) 
-  return Memoize(lambda t: np.asmatrix(sliced(t)))
+  return (lambda t: np.asmatrix(sliced(t)))
 
 def filt_slice(mu, stim_filter, hist_filter):
   sliced = np.concatenate(([mu], stim_filter, hist_filter))
   return np.asmatrix(sliced)
 
 def logI(t, theta, data):
-  return theta * data(t).T
+  return np.reshape(theta * data(t).T,[1])
 
 def logL(theta, data, delta, time, size, sp_times):
   intensities = [logI(t, theta, data) for t in range(size,time)]
@@ -68,7 +79,7 @@ def testopt():
   theta = filt_slice(1, sf, hf)
   data = data_slicer(size, stim, spikes)
   args = (data, delta, time, size, times)
-  hess_p = lambda th, p, *args: logL_hess(th, *args) * p
+  hess_p = lambda th, p, *args: np.reshape( logL_hess(th, *args) * p,[p.size,p.size])
   x = opt.fmin_ncg(logL, theta, fprime=logL_grad, fhess_p=hess_p, args=args)
   return x
 
@@ -77,13 +88,13 @@ def testopt():
 time = 1000
 size = 40
 stim = cos_stimulus(time,200)
-spikes = spike_train(stim)
 times = filter(lambda t: t > size, spike_times(spikes))
-sf = rand_filter(size)
-hf = rand_filter(size)
-delta = 1
+sf = cos_filter(size)
+hf = cos_filter(size)
+delta = 0.005
+spikes = poiss_train(theta,stim,delta,time,size)
 theta = filt_slice(1, sf, hf)
-data = data_slicer(size, stim, spikes)
+data = Memoize(data_slicer(size, stim, spikes))
 args = (data, delta, time, size, times)
 
 
