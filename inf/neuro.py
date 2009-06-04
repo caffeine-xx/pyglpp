1f9a8e12985b76f4b12da423edb5df702c189fea
Dan <dan@dan-laptop.(none)>
2009-06-04 18:49:35 +0100

    reshape fixed mismatched size error

--- inf/neuro.py
+++ inf/neuro.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.optimize as opt
 import random as rd
 import math
-
+from memoize import *
 
 def cos_stimulus(time,period):
   return np.asarray(map(lambda t: math.cos(t*math.pi*2/period)**2,range(0,time)))
@@ -17,10 +17,14 @@ def cos_filter(size):
   base = map(lambda i: i * math.pi / (2*size), range(0,size))
   return map(lambda i: math.cos(i), base)
 
+def rand_filter(size):
+  return [rd.random() for i in range(size)]
+
+
 def data_slicer(size, stim, spikes):
   start = lambda t: max(t-size,0)
   sliced =lambda t: np.concatenate(([1], stim[start(t):t], spikes[start(t):t])) 
-  return lambda t: np.asmatrix(sliced(t))
+  return Memoize(lambda t: np.asmatrix(sliced(t)))
 
 def filt_slice(mu, stim_filter, hist_filter):
   sliced = np.concatenate(([mu], stim_filter, hist_filter))
@@ -33,12 +37,13 @@ def logL(theta, data, delta, time, size, sp_times):
   intensities = [logI(t, theta, data) for t in range(size,time)]
   term1 = sum([intensities[t-size] for t in sp_times])
   term2 = sum([delta*math.exp(intensities[t-size]) for t in range(size,time)])
-  return term1-term2
+  return np.array(term1-term2)
 
 def logL_grad(theta, data, delta, time, size, sp_times):
   term1 = sum([data(t) for t in sp_times])
   term2 = sum([delta * math.exp(logI(t,theta,data)) * data(t) for t in range(size,time)])
-  return term1-term2
+  result = np.array(term1-term2)
+  return np.reshape(result, [result.size])
 
 def logL_hess(theta, data, delta, time, size, sp_times):
   datm = lambda t: np.matrix(data(t))
@@ -74,8 +79,8 @@ size = 40
 stim = cos_stimulus(time,200)
 spikes = spike_train(stim)
 times = filter(lambda t: t > size, spike_times(spikes))
-sf = cos_filter(size)
-hf = cos_filter(size)
+sf = rand_filter(size)
+hf = rand_filter(size)
 delta = 1
 theta = filt_slice(1, sf, hf)
 data = data_slicer(size, stim, spikes)
