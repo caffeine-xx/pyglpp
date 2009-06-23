26b338bd2bf6e9fd0bf8c5121549589ac94a7cbe
Dan <dan@dan-laptop.(none)>
2009-06-23 18:44:05 +0100

    added neuro tools spike monitor

--- brian_trial.py
+++ brian_trial.py
@@ -1,22 +1,56 @@
 from brian import *
 from brian.library.IF import *
-from brian.library.synapses import *
-import time
 
-def single_izhikevich_trial():
+class NeuroToolsSpikeMonitor(FileSpikeMonitor):
+  ''' Spike monitor that outputs a NeuroTools-compatible format '''
+  def __init__(self,source,filename,record=False,delay=0):
+      super(FileSpikeMonitor,self).__init__(source,record,delay)
+      self.filename = filename
+      self.f = open(filename,'w')
+      self.write_header()
+  
+  def reinit(self):
+      self.close_file()
+      self.f = open(self.filename,'w')
+      self.write_header()
+  
+  def propagate(self,spikes):
+    super(NeuroToolsSpikeMonitor,self).propagate(spikes)
+    for i in spikes:
+      self.write(str(float(self.source.clock.t*1000))+"\t"+str(i)+"\n")
+  
+  def write_header(self):
+    header =  "# dt = %s\n" + str(float(self.source.clock.dt))
+    header += "# first_id = 0\n"
+    header += "# last_id = %i\n" % str(int(self.source.N))
+    self.write(header)
+
+def single_izhikevich_trial(a=0.2/ms,b=0.2/ms,time=100*ms,prefix='results/single_izhikevich_trial'):
+  ''' Runs a 1000ms trial of an izhikevich neuron with a single
+      randomly rate-varying Poisson input, centered at 40Hz.
+      Outputs results of stimulus and neuron firing to:
+        prefix/N_in.dat
+        prefix/N_out.dat '''
+  
   # Izhikevich model with excitatory synapse
-  model = Izhikevich()
-  reset = AdaptiveReset(Vr=-75*mV, b=0.2/ms)
-  neuron = NeuronGroup(10,model=model, threshold=-30.0*mV, reset=reset)
+  model  = Izhikevich(a,b)
+  reset  = AdaptiveReset(Vr=-75*mV, b=0.2/ms)
+  neuron = NeuronGroup(1,model=model, threshold=-30.0*mV, reset=reset)
+    
   # Poisson stimulus
-  rates = lambda t: max(0,normal(40.0*Hz, 20.0*Hz))
-  stimulus = PoissonGroup(1,rates=rates)
-  connection = Connection(stimulus, neuron)
-  connection[0,:] = 80*mvolt
+  rates           = lambda t: max(0,normal(40.0*Hz, 20.0*Hz))
+  stimulus        = PoissonGroup(1,rates=rates)
+  connection      = Connection(stimulus, neuron)
+  connection[0,:] = 40*mvolt
+  
   # Spike recording
-  neuron_monitor = SpikeMonitor(neuron,record=True)
-  input_monitor = SpikeMonitor(stimulus,record=True)
-  run(1000*ms)
-  raster_plot(input_monitor)
-  show()
-  return neuron
+  in_file  = "%s_in.dat"  % (prefix)
+  out_file = "%s_out.dat" % (prefix)
+  
+  in_monitor  = NeuroToolsSpikeMonitor(stimulus,in_file,record=True)
+  out_monitor = NeuroToolsSpikeMonitor(neuron,out_file,record=True)
+  
+  neuron.clock.reinit()
+  run(time)
+
+
