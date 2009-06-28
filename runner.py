283ee724cc52160ac2f3e28baecff4672c8616de
Daniel Cook <dc1108@doc.ic.ac.uk>
2009-06-28 14:43:25 +0100

    moved imports

--- runner.py
+++ runner.py
@@ -1,6 +1,7 @@
+import trials
+import cPickle
+
 def run_trial(prefix, id):
-  import trials
-  import cPickle
   param_generator = getattr(trials,prefix+"_params")
   params = param_generator(id)
   trial_runner = getattr(trials, prefix+"_trial")
