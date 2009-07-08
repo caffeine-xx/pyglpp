7592e7cd85e00b48abf844e32becca3cbdeb7751
Dan <dan@dan-laptop.(none)>
2009-07-08 10:50:48 +0100

    added inhibitory neurons, tweaked default sim a bit

--- simulations.py
+++ simulations.py
@@ -2,6 +2,7 @@ from numpy import *
 from brian import *
 from brian.library.IF import *
 from signals import *
+import cPickle
 
 class Simulator:
   ''' Wrapper for the Brian simulator '''
@@ -12,44 +13,48 @@ class Simulator:
         I      = ge*(Ee-v) - gi*(Ei-v) : amp
         dge/dt = -ge / te : siemens
         dgi/dt = -gi / ti : siemens'''
-
+   
   def __init__(self, model  ={'a':0.02,        'b':0.2, 
                               'Ee':0*mV,       'Ei':-80.0*mV, 
                               'te':10*ms,      'ti':10*ms,
-                              'C_m':1.3*pF                 },
+                              'C_m':2.0*pF                 },
                      init   ={'gei':10*nS,     'gii':20*nS },
-                     neurons={'N':10, 
+                     neurons={'N':20,          'Ni': 7,
                               'threshold':     'v > 30.0*mV',
                               'reset':         'v = -65.0*mV;'+
                                                'w += 8.0*mV'},
-                     connect={'weight':0.2*nS, 'sparseness':0.5,
-                              'state':'ge'                 }):
-    
+                     connect={'weight':0.1*nS, 'sparseness':0.1,
+                              'state':'ge'                 },
+                     inhibit={'weight':0.1*nS, 'sparseness':0.2,
+                              'state':'gi'}):
+    (N, Ni) = (neurons['N'], neurons['Ni'])
     self.params  = {'model':model,     'neurons':neurons, 
-                    'connect':connect, 'init':init}
+                    'connect':connect, 'inhibit':inhibit, 'init':init}
     self.model   = Equations(self.eqs, **model)
     self.neurons = NeuronGroup(model=self.model,**neurons)
-    self.connect = Connection(self.neurons, self.neurons, **connect)
-
+    self.connect = Connection(self.neurons[Ni:], self.neurons, **connect)
+    self.inhibit = Connection(self.neurons[:Ni], self.neurons, **inhibit)
+    
   def run(self, signal=GaussianNoiseGenerator(40.0, 10.0, 10).generate(Trial(0.0,100.0,0.1)), 
                 connect_in={'state':'ge','weight':10*nS, 'delay':1*ms}):
-    ''' Runs a trial of the simulation using the 
-        input signal provided '''
-    self.params['connect_in'] = connect_in
-     
+    ''' Runs a trial of the simulation using the input signal provided '''
+    trial       = signal.trial
     input       = PoissonGroup(signal.dims(), rates=lambda t: signal[t])
     in_conn     = Connection(input, self.neurons, **connect_in)
     in_monitor  = SpikeMonitor(input, record=True)
     out_monitor = SpikeMonitor(self.neurons, record=True)
-        v_mon   = StateMonitor(self.neurons, 'v', record=True)
-    network     = Network(self.neurons, self.connect, input,
-                          in_conn, in_monitor, out_monitor)
+    v_mon       = StateMonitor(self.neurons, 'v', record=True)
+    network     = Network(self.neurons, self.connect, self.inhibit,
+                          input, in_conn, in_monitor, out_monitor,
+                          v_mon)
     network.reinit()
     network.run(trial.duration()*ms)
-
+    
+    self.params['connect_in'] = connect_in
+    print v_mon.values
+    
     spikes_in  = self.monitor_to_signal(trial, signal.dims(), in_monitor)
     spikes_out = self.monitor_to_signal(trial, self.params['neurons']['N'], out_monitor)
-    
     result = Result(self.params, signal, spikes_in, spikes_out)
     return result
   
@@ -67,6 +72,7 @@ class Result:
         - output: Tuple of output signals from neuron populations (usually
             input Poisson population and output Izhikevich population)
         - trial:  Clock information '''
+
   def __init__(self, params, signal, input, output):
     self.params = params
     self.signal = signal
@@ -74,12 +80,5 @@ class Result:
     self.output = output 
   
   def write_to_file(self,filename):
-    # TODO: Implement
-    raise Exception("Unimplemented method")
-
-def run_simulation(input_signal, simulator, times=1):
-  for t in trials:
-    simulator.run(input_signal)
-  simulator.write('output.dat')
-
-
+    cPickle.Pickler(file(filename,'w'),2).dump(self)
+  
